68c803d247b71584a030c2556dced9110cdabf3e
mussrya <ryan.musselwhite@objectivemanager.com>
2017-01-19 19:49:33 +0000

    Adding mobile support

--- app/components/TopMoney.js
+++ app/components/TopMoney.js
@@ -17,7 +17,7 @@ class TopMoneyBlock extends React.Component {
                         {
                             return <div className="col-6 col-sm-3 text-center" key={index}>
                                 <div className="whiteBox">
-                                    <h4 className="text-muted padding-top-15">{item.name}</h4>
+                                    <h4 className="text-center boxHeading padding-top-15">{item.name}</h4>
                                     <h1 className="h1--value">£{Comma(item.value)}</h1>
                                 </div>
                             </div>

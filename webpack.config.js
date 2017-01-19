103ca11a229f0b7f27caa292610781d2cb4b631b
mussrya <ryan.musselwhite@objectivemanager.com>
2017-01-19 21:27:05 +0000

    Fixing webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 // In webpack.config.js
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let webpack = require('webpack');
 
-var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: __dirname + '/app/index.html',
-    filename: 'index.html',
-    inject: 'body'
-});
+let HTMLWebpackPluginConfig = new HtmlWebpackPlugin(
+    {
+        template: __dirname + '/app/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    }
+);
 
 module.exports = {
     entry: [
@@ -22,16 +24,21 @@ module.exports = {
             {test: /\.css$/, exclude: /node_modules/, loader: "style-loader!css-loader"}
         ]
     },
-    plugins:[HtmlWebpackPlugin,
-    new webpack.DefinePlugin({
-        'process.env':{
-            'NODE_ENV': JSON.stringify('production')
-        }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-        compress:{
-            warnings: true
-        }
-    })
-]
+    plugins: [
+        HtmlWebpackPlugin,
+        new webpack.DefinePlugin(
+            {
+                'process.env': {
+                    'NODE_ENV': JSON.stringify('production')
+                }
+            }
+        ),
+        new webpack.optimize.UglifyJsPlugin(
+            {
+                compress: {
+                    warnings: true
+                }
+            }
+        )
+    ]
 };
